785e5c3e98b6bfccfe2ae6f96683ce3ad89bc9f3
kngu9 <me@khanh.pw>
2019-03-18 01:23:00 -0200

    Added KSQL charm

---
+++ Makefile
@@ -0,0 +1,42 @@
+KSQL_VERSION := $(shell awk '/version:/ {print $$2}' ksql-server/snap/snapcraft.yaml | head -1 | sed "s/'//g")
+
+.PHONY: all
+all: snap charm
+
+.PHONY: dev
+dev: snap fat-charm
+
+.PHONY: snap
+snap: ksql-server/ksql-server_$(KSQL_VERSION)_amd64.snap
+
+ksql-server/ksql-server_$(KSQL_VERSION)_amd64.snap:
+	(cd ksql-server; SNAPCRAFT_BUILD_ENVIRONMENT_MEMORY=6G snapcraft)
+
+.PHONY: charm
+charm: charm/builds/ksql
+
+.PHONY: fat-charm
+fat-charm: ksql-server/ksql-server_$(KSQL_VERSION)_amd64.snap ./charm/builds/ksql
+	cp -rf $< charm/ksql
+	$(MAKE) -C charm/ksql
+
+charm/builds/ksql:
+	$(MAKE) -C charm/ksql
+
+.PHONY: clean
+clean: clean-charm clean-snap
+
+.PHONY: clean-charm
+clean-charm:
+	$(RM) -r charm/builds charm/deps
+	$(RM) charm/ksql/*.snap
+
+.PHONY: clean-snap
+clean-snap:
+	(cd ksql-server; snapcraft clean)
+	
+sysdeps: /snap/bin/charm /snap/bin/snapcraft
+/snap/bin/charm:
+	sudo snap install charm --classic
+/snap/bin/snapcraft:
+	sudo snap install snapcraft --classic
--- ksql-server/snap/local/ksql-server-wrapper.bash
+++ ksql-server/snap/local/ksql-server-wrapper.bash
@@ -2,22 +2,22 @@
                                
 set -eu
 
-mkdir -p $SNAP_DATA/etc $SNAP_DATA/log $SNAP_DATA/kafka-streams
+mkdir -p $SNAP_COMMON/etc $SNAP_COMMON/log $SNAP_COMMON/kafka-streams
 
-if [ ! -f $SNAP_DATA/etc/ksql-server.properties ]; then
-	cat >$SNAP_DATA/etc/ksql-server.properties <<EOF
+if [ ! -f $SNAP_COMMON/etc/ksql-server.properties ]; then
+	cat >$SNAP_COMMON/etc/ksql-server.properties <<EOF
 bootstrap.servers=localhost:9092
 listeners=http://localhost:8088
-state.dir=$SNAP_DATA/kafka-streams
+state.dir=$SNAP_COMMON/kafka-streams
 EOF
 fi
-if [ ! -f $SNAP_DATA/etc/log4j.properties ]; then
-	cp $SNAP/etc/ksql/log4j.properties $SNAP_DATA/etc
+if [ ! -f $SNAP_COMMON/etc/log4j.properties ]; then
+	cp $SNAP/etc/ksql/log4j.properties $SNAP_COMMON/etc
 fi
 
-export KSQL_LOG4J_OPTS="-Dlog4j.configuration=file:$SNAP_DATA/etc/log4j.properties"
-export LOG_DIR=$SNAP_DATA/log
+export KSQL_LOG4J_OPTS="-Dlog4j.configuration=file:$SNAP_COMMON/etc/log4j.properties"
+export LOG_DIR=$SNAP_COMMON/log
 export PATH=$SNAP/usr/lib/jvm/default-java/bin:$PATH
 unset JAVA_HOME
 
-$SNAP/usr/bin/ksql-server-start $SNAP_DATA/etc/ksql-server.properties
+$SNAP/usr/bin/ksql-server-start $SNAP_COMMON/etc/ksql-server.properties
