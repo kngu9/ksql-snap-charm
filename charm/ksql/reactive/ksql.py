3b10a251de4e67d6e0a97936a0b2bbc412e1b260
kngu9 <khanhtnguyen300@gmail.com>
2019-03-25 10:40:09 +0000

    Added snap-layer

--- charm/ksql/reactive/ksql.py
+++ charm/ksql/reactive/ksql.py
@@ -13,257 +13,82 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import socket
-import tempfile
-import glob
-import subprocess
+from charms.layer.ksql import Ksql, KSQL_SNAP_COMMON
 
-from pathlib import Path
-from OpenSSL import crypto
-from subprocess import check_call
-
-from charms.reactive import set_state, remove_state, when, when_not, hook, when_file_changed
 from charmhelpers.core import hookenv
+
+from charms.reactive import (set_state, remove_state, when, when_not,
+                             hook, clear_flag, is_flag_set, set_flag,
+                             when_any)
 from charms.reactive.helpers import data_changed
-from charmhelpers.core.hookenv import log, config
 
-from charms.layer.ksql import KSQL_SNAP, KSQL_SNAP_COMMON, KSQL_PORT, Ksql
-from charms.layer import tls_client
+from charmhelpers.core.hookenv import log
 
-@when_not('ksql.available')
-def install():
-    install_snap()
 
-@hook('upgrade-charm')
-def upgrade():
-    upgrade_snap()
+@hook('config-changed')
+def config_changed():
+    remove_state('ksql.configured')
+    set_flag('ksql.force-reconfigure')
 
-@hook('stop')
-def uninstall():
-    try:
-        check_call(['snap', 'remove', KSQL_SNAP])
-    except subprocess.CalledProcessError as e:
-        hookenv.log("failed to remove snap: {}".format(e))
 
-def install_snap():
-    # Need to install the core snap explicit. If not, there's
-    # no slots for removable-media on a bionic install.
-    # Not sure if that's a snapd bug or intended behavior.
-    check_call(['snap', 'install', 'core'])
-
-    cfg = config()
-    # KSQL-Server's snap presedence is:
-    # 1. Included in snap
-    # 2. Included with resource of charm of 'ksql-server'
-    # 3. Snap store with release channel specified in config
-    snap_file = get_snap_file_from_charm() or hookenv.resource_get('ksql-server')
-    if snap_file:
-        check_call(['snap', 'install', '--dangerous', snap_file])
-    if not snap_file:
-        check_call(['snap', 'install', '--{}'.format(cfg['ksql-release-channel']), KSQL_SNAP])
-    
-    set_state('ksql.available')
-
-def upgrade_snap():
-    cfg = config()
-    check_call(['snap', 'refresh', '--{}'.format(cfg['ksql-release-channel']), KSQL_SNAP])
-    set_state('ksql.available')
-
-@when('ksql.available')
-@when_not('kafka.joined')
-def waiting_for_kafka():
+@when('snap.installed.ksql-server')
+@when_not('ksql.configured')
+def configure():
     ksql = Ksql()
-    ksql.close_ports()
-    ksql.stop()
-    hookenv.status_set('blocked', 'waiting for relation to kafka')
+    kafkas = ksql.get_kafkas()
 
-@when_not(
-    'ksql.ca.keystore.saved',
-    'ksql.server.keystore.saved',
-)
-@when('ksql.available')
+    if data_changed('ksql.kafka_units', kafkas) or is_flag_set('ksql.force-reconfigure'):
+        ksql.install(kafka_units=kafkas)
+        ksql.open_ports()
+
+    clear_flag('ksql.force-reconfigure')
+    set_flag('ksql.available')
+    set_state('ksql.configured')
+
+    hookenv.application_version_set(ksql.version())
+
+
+@when('snap.installed.ksql-server')
+@when_not('ksql.client.keystore.saved')
 def waiting_for_certificates():
+    remove_state('ksql.available')
     hookenv.status_set('waiting', 'waiting for easyrsa relation')
 
+
+@when('snap.installed.ksql-server')
+@when_not('kafka.ready')
+def waiting_for_kafka():
+    remove_state('ksql.available')
+    hookenv.status_set('waiting', 'waiting for kafka relation')
+
+
 @when(
-    'ksql.available',
-    'kafka.ready',
-    'ksql.ca.keystore.saved',
+    'snap.installed.ksql-server',
+    'ksql.ready',
     'ksql.client.keystore.saved'
 )
-@when_not('ksql.started')
-def configure_ksql(kafka):
+@when_not('ksql.available')
+def configure_kafka(kafka):
     hookenv.status_set('maintenance', 'setting up ksql')
 
-    ksql = Ksql()
-    ksql.configure(kafka.kafkas())
-    ksql.open_ports()
-    set_state('ksql.started')
-    hookenv.status_set('active', 'ready')
-    # set app version string for juju status output
-    ksql_version = ksql.version() or 'unknown'
-    hookenv.application_version_set(ksql_version)
+    remove_state('ksql.configured')
+    set_flag('ksql.force-reconfigure')
 
-@when_file_changed(
-    os.path.join(KSQL_SNAP_COMMON, 'etc', "ksql.client.jks"),
-    os.path.join(KSQL_SNAP_COMMON, 'etc', 'ksql-server.properties')
+
+@when(
+    'ksql.configured'
+    'kafka.available'
 )
-def restart_when_files_changed():
-    hookenv.status_set('maintenance', 'restarting ksql')
-    Ksql().restart()
-    hookenv.status_set('active', 'ready')
+def configure_ksql_kafkas(kafka):
+    """
+    As kafka brokers come and go, ksql-server.properties will be updated.
+    When that file changes, restart Kafka and set appropriate status messages.
+    """
+    kafkas = kafka.kafkas()
 
-@when('certificates.available')
-def get_client_cert():
-    # Request client certs from easyrsa
-    tls_client.request_client_cert(
-        'system:snap-ksql-server',
-        crt_path=os.path.join(
-            KSQL_SNAP_COMMON,
-            'etc',
-            'ksql-server.client.crt',
-        ),
-        key_path=os.path.join(
-            KSQL_SNAP_COMMON,
-            'etc',
-            'client.key'
-        )
-    )
+    if not data_changed('ksql.kafka_units', kafkas):
+        return
 
-@when('tls_client.certs.changed')
-def import_srv_crt_to_keystore():
-    keystore_path = os.path.join(
-        KSQL_SNAP_COMMON,
-        'etc',
-        "ksql.client.jks"
-    )
-    keystore_password = _keystore_password()
-    crt_path = os.path.join(
-        KSQL_SNAP_COMMON,
-        'etc',
-        'ksql-server.client.crt'
-    )
-    key_path = os.path.join(
-        KSQL_SNAP_COMMON,
-        'etc',
-        "client.key"
-    )
-    if os.path.isfile(crt_path) and os.path.isfile(key_path):
-        with open(crt_path, 'rt') as f:
-            cert = f.read()
-            loaded_cert = crypto.load_certificate(
-                crypto.FILETYPE_PEM,
-                cert
-            )
-        
-        if not data_changed('ksql_ca_certificate', cert):
-            return
-        log('server certificate changed')
-
-        with open(key_path, 'rt') as f:
-            key = f.read()
-            loaded_key = crypto.load_privatekey(
-                crypto.FILETYPE_PEM,
-                key
-            )
-
-        pkcs12 = crypto.PKCS12Type()
-        pkcs12.set_certificate(loaded_cert)
-        pkcs12.set_privatekey(loaded_key)
-        pkcs12_data = pkcs12.export(keystore_password)
-        fd, path = tempfile.mkstemp()
-        log('opening tmp file {}'.format(path))
-        try:
-            with os.fdopen(fd, 'wb') as tmp:
-                # write cert and private key to the pkcs12 file
-                tmp.write(pkcs12_data)
-                log('Writing pkcs12 temporary file {0}'.format(
-                    path
-                ))
-                tmp.close()
-                log('importing pkcs12')
-                # import the pkcs12 into the keystore
-                check_call(
-                    '/snap/{snap_name}/current/usr/lib/jvm/default-java/bin/keytool -v -importkeystore -srckeystore {path} -srcstorepass {password} -srcstoretype PKCS12 -destkeystore {keystore} -deststoretype JKS -deststorepass {password} --noprompt'.format(
-                        snap_name=KSQL_SNAP,
-                        path=path,
-                        password=keystore_password,
-                        keystore=keystore_path
-                    ),
-                    shell=True
-                )
-                os.chmod(keystore_path, 0o440)
-                remove_state('tls_client.certs.changed')
-                set_state('ksql.client.keystore.saved')
-        finally:
-            os.remove(path)
-    else:
-        log('server certificate of key file missing'.format(
-            cert=os.path.isfile(crt_path),
-            key=os.path.isfile(key_path)
-        ))
-
-
-@when('tls_client.ca_installed')
-@when_not('ksql.ca.keystore.saved')
-def import_ca_crt_to_keystore():
-    service_name = hookenv.service_name()
-    ca_path = '/usr/local/share/ca-certificates/{0}.crt'.format(service_name)
-
-    if os.path.isfile(ca_path):
-        changed = False
-
-        with open(ca_path, 'rt') as f:
-            ca_cert = f.read()
-            changed = data_changed('ca_certificate', ca_cert)
-
-        if changed:
-            ca_keystore = os.path.join(
-                KSQL_SNAP_COMMON,
-                'etc',
-                "ksql.client.truststore.jks"
-            )
-            password = _keystore_password()
-            check_call(
-                '/snap/{snap_name}/current/usr/lib/jvm/default-java/bin/keytool -import -trustcacerts -keystore {keystore} -storepass  {keystorepass} -file {path} -noprompt'.format(
-                    snap_name=KSQL_SNAP,
-                    path=ca_path,
-                    keystore=ca_keystore,
-                    keystorepass=password
-                ),
-                shell=True
-            )
-            os.chmod(ca_keystore, 0o440)
-            remove_state('tls_client.ca_installed')
-            set_state('ksql.ca.keystore.saved')
-
-def _keystore_password():
-    path = os.path.join(
-        KSQL_SNAP_COMMON,
-        'etc',
-        "keystore.secret"
-    )
-    if not os.path.isfile(path):
-        _ensure_directory(path)
-        check_call(
-            ['head -c 32 /dev/urandom | base64 > {}'.format(path)],
-            shell=True
-        )
-        os.chmod(path, 0o440)
-    password = Path(path).read_text()
-    return password.rstrip()
-
-
-def _ensure_directory(path):
-    '''Ensure the parent directory exists creating directories if necessary.'''
-    directory = os.path.dirname(path)
-    if not os.path.isdir(directory):
-        os.makedirs(directory)
-    os.chmod(directory, 0o770)
-
-def get_snap_file_from_charm():
-    snap_files = sorted(glob.glob(os.path.join(hookenv.charm_dir(), "{}*.snap".format(KSQL_SNAP))))[::-1]
-    if not snap_files:
-        return None
-    return snap_files[0]
+    log('kafka(s) joined, forcing reconfiguration')
+    remove_state('ksql.configured')
+    set_flag('ksql.force-reconfigure')
---
+++ charm/ksql/reactive/tls.py
@@ -0,0 +1,146 @@
+import os
+import tempfile
+
+from OpenSSL import crypto
+from subprocess import check_call
+
+from charms.layer import tls_client
+from charms.layer.ksql import (keystore_password, KSQL_SNAP_COMMON,
+                               KSQL_KEYTOOL_PATH)
+
+from charms.reactive import (when, when_file_changed, remove_state,
+                             when_not, set_state, set_flag)
+from charms.reactive.helpers import data_changed
+
+from charmhelpers.core import hookenv
+
+from charmhelpers.core.hookenv import log
+
+
+@when('certificates.available')
+def send_data():
+    # Request a server cert with this information.
+    tls_client.request_client_cert(
+        'system:snap-kafka',
+        crt_path=os.path.join(
+            KSQL_SNAP_COMMON,
+            'etc',
+            'client.crt',
+        ),
+        key_path=os.path.join(
+            KSQL_SNAP_COMMON,
+            'etc',
+            'client.key'
+        )
+    )
+
+
+@when_file_changed(
+    os.path.join(KSQL_SNAP_COMMON, 'etc', 'ksql.client.jks'),
+    os.path.join(KSQL_SNAP_COMMON, 'etc', 'ksql.client.truststore.jks')
+)
+def restart_when_keystore_changed():
+    remove_state('kafka.configured')
+    set_flag('kafka.force-reconfigure')
+
+
+@when('tls_client.certs.changed')
+def import_srv_crt_to_keystore():
+    password = keystore_password()
+    crt_path = os.path.join(
+        KSQL_SNAP_COMMON,
+        'etc',
+        'client.crt'
+    )
+    key_path = os.path.join(
+        KSQL_SNAP_COMMON,
+        'etc',
+        'client.key'
+    )
+
+    if os.path.isfile(crt_path) and os.path.isfile(key_path):
+        with open(crt_path, 'rt') as f:
+            cert = f.read()
+            loaded_cert = crypto.load_certificate(
+                crypto.FILETYPE_PEM,
+                cert
+            )
+            if not data_changed(
+                'kafka_client_certificate',
+                cert
+            ):
+                log('server certificate of key file missing')
+                return
+
+        with open(key_path, 'rt') as f:
+            loaded_key = crypto.load_privatekey(
+                crypto.FILETYPE_PEM,
+                f.read()
+            )
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            log('server certificate changed')
+
+            keystore_path = os.path.join(
+                KSQL_SNAP_COMMON,
+                'etc',
+                'ksql.client.jks'
+            )
+
+            pkcs12 = crypto.PKCS12Type()
+            pkcs12.set_certificate(loaded_cert)
+            pkcs12.set_privatekey(loaded_key)
+            pkcs12_data = pkcs12.export(password)
+            log('opening tmp file {}'.format(tmp.name))
+
+            # write cert and private key to the pkcs12 file
+            tmp.write(pkcs12_data)
+            tmp.flush()
+
+            log('importing pkcs12')
+            # import the pkcs12 into the keystore
+            check_call([
+                KSQL_KEYTOOL_PATH,
+                '-v', '-importkeystore',
+                '-srckeystore', str(tmp.name),
+                '-srcstorepass', password,
+                '-srcstoretype', 'PKCS12',
+                '-destkeystore', keystore_path,
+                '-deststoretype', 'JKS',
+                '-deststorepass', password,
+                '--noprompt'
+            ])
+            os.chmod(keystore_path, 0o440)
+
+            remove_state('tls_client.certs.changed')
+            set_state('ksql.client.keystore.saved')
+
+
+@when('tls_client.ca_installed')
+@when_not('ksql.ca.keystore.saved')
+def import_ca_crt_to_keystore():
+    ca_path = '/usr/local/share/ca-certificates/{}.crt'.format(
+        hookenv.service_name()
+    )
+
+    if os.path.isfile(ca_path):
+        with open(ca_path, 'rt') as f:
+            changed = data_changed('ca_certificate', f.read())
+
+        if changed:
+            ca_keystore = os.path.join(
+                KSQL_SNAP_COMMON,
+                'etc',
+                'ksql.client.truststore.jks'
+            )
+            check_call([
+                KSQL_KEYTOOL_PATH,
+                '-import', '-trustcacerts', '-noprompt',
+                '-keystore', ca_keystore,
+                '-storepass', keystore_password(),
+                '-file', ca_path
+            ])
+            os.chmod(ca_keystore, 0o440)
+
+            remove_state('tls_client.ca_installed')
+            set_state('ksql.ca.keystore.saved')
